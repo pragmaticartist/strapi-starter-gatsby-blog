9e2c287d975a1980d7b338ca11e28ba1f7fc300d
Tiffany <32379860+pragmaticartist@users.noreply.github.com>
2020-12-27 03:45:31 -0800

    Add local fonts

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,34 +2,49 @@ import React from "react";
 import styled from "styled-components";
 import "../styles/index.css";
 
-const Container = styled.div`
-  width: 50vw;
+const BlogContainer = styled.div`
+  width: 40vw;
   min-width: 300px;
-  max-width: 1240px;
+  max-width: 1080px;
   margin: 0 auto;
+
+  font-family: Lora;
   color: #111111;
 `;
 
 const BlogTitleSvg = styled.svg`
   width: 100%;
-  margin: 40px 0;
+  margin: 40px 0 77px;
 
   font-family: ImmortalMemory;
 `;
 
-const FeedContainer = styled.div``;
+const WritingsContainer = styled.div`
+  font-size: 1.5rem;
+  line-height: 2.5rem;
+`;
+
+const WritingTitle = styled.h1`
+  margin-bottom: 25px;
+  font-weight: bold;
+`;
 
 const IndexPage = () => (
-  <Container>
+  <BlogContainer>
     <BlogTitleSvg viewBox='0 0 73 20'>
       <text x='1' y='13'>
         Pragmatic Artist
       </text>
     </BlogTitleSvg>
-    <FeedContainer>
-      <p>First blog post on timeline</p>
-    </FeedContainer>
-  </Container>
+    <WritingsContainer>
+      <WritingTitle>First blog post on timeline</WritingTitle>
+      <p>
+        Writings will look like this. Super awesome! Add some more words and it
+        gets even better. Type quietly though. People are sleeping because
+        you're up at 3 in the morning dingus.
+      </p>
+    </WritingsContainer>
+  </BlogContainer>
 );
 
 export default IndexPage;
