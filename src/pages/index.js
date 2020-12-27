fca6131d212877c4dec3650f6b32ca89508a31a6
Tiffany <32379860+pragmaticartist@users.noreply.github.com>
2020-12-26 23:59:34 -0800

    Add styled-components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,32 @@
 import React from "react";
-import { useStaticQuery } from "gatsby";
+import styled from "styled-components";
 import "../assets/css/main.css";
 
+const Container = styled.div`
+  width: 50vw;
+  min-width: 500px;
+  max-width: 98vw;
+  margin: 0 auto;
+  color: #111111;
+`;
+
+const BlogTitle = styled.h1`
+  margin: 40px 0;
+  font-size: 1em;
+`;
+
+const FeedContainer = styled.div`
+  width: 44vw;
+  margin: 0 auto;
+`;
+
 const IndexPage = () => (
-  <div>
-    <h1 style={{ textAlign: "center" }}>Pragmatic Artist</h1>
-  </div>
+  <Container>
+    <BlogTitle style={{ textAlign: "center" }}>Pragmatic Artist</BlogTitle>
+    <FeedContainer>
+      <p>First blog post on timeline</p>
+    </FeedContainer>
+  </Container>
 );
 
 export default IndexPage;
